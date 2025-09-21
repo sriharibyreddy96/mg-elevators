d418bbf284c9b30ee46b9e1471e64fbbc4069f8c
sriharibyreddy96 <sriharibyreddy96@gmail.com>
2025-09-21 10:42:34 +0530

    deploy Changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export", // enable static HTML export
+  output: "export", // enable static HTML export
 };
 
 export default nextConfig;
