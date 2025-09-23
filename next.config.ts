a149839b22095e9bba0e84c2c4290dbeb264f0d5
sriharibyreddy96 <sriharibyreddy96@gmail.com>
2025-09-23 10:49:49 +0530

    Mapping Points Commit

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export", // enable static HTML export
-
-  // GitHub Pages needs correct asset paths
+  output: "export",
   basePath: isProd ? "/mg-elevators" : "",
   assetPrefix: isProd ? "/mg-elevators/" : "",
-
-  // GitHub Pages does not support Next.js image optimization
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
