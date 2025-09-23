dea4fc7def89680061b2525e13df529946843eec
sriharibyreddy96 <sriharibyreddy96@gmail.com>
2025-09-23 12:42:53 +0530

    Config Changes

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/mg-elevators" : "",
   assetPrefix: isProd ? "/mg-elevators/" : "",
+  trailingSlash: true, // Optional, but good for static sites
+  outputFileTracingRoot: __dirname, // Prevents workspace root warnings
 };
 
 export default nextConfig;
