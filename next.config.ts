0ce20464ca0d04d9218be4777cc51794b1450d11
sriharibyreddy96 <sriharibyreddy96@gmail.com>
2025-09-24 12:00:59 +0530

    Config Export Changes

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,13 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  // output: "export",
+  output: "export", // enable static export for GitHub Pages
   basePath: isProd ? "/mg-elevators" : "",
   assetPrefix: isProd ? "/mg-elevators/" : "",
-  trailingSlash: true, // Optional, but good for static sites
-  outputFileTracingRoot: __dirname, // Prevents workspace root warnings
+  trailingSlash: true, // recommended for static hosting
+  images: {
+    unoptimized: true, // required for GitHub Pages
+  },
 };
 
 export default nextConfig;
